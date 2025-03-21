b188b698f57344dd6a5efc88b1725cd2049c4920
Ash2623 <ayushranjha143@gmail.com>
2025-03-22 00:52:21 +0530

    Delete frontend directory

--- frontend/script.js
+++
@@ -1,9 +0,0 @@
-fetch('http://127.0.0.1:5000/')  // Ensure this matches the Flask backend URL
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("response").innerText = data.message;
-    })
-    .catch(error => {
-        console.error("Error fetching data:", error);
-        document.getElementById("response").innerText = "Error fetching data!";
-    });
