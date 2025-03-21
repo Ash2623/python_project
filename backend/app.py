ca6490cf3fd769315bbf0039220f20290cce84fb
Ayush <ayushranjha143@gmail.com>
2025-03-21 23:53:03 +0530

    Initial commit

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')  # Make sure this route exists
+def home():
+    return jsonify({"message": "Welcome to the Flask Backend!"})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
+
