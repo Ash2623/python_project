5006d5abddde59905ee3a978d2a7e444ad1496d2
Ayush <ayushranjha143@gmail.com>
2025-03-24 18:11:16 +0530

    es

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ CORS(app)
 
 @app.route('/')  # Make sure this route exists
 def home():
-    return jsonify({"message": "Welcome to the fuss Busss!"})
+    return jsonify({"message": "Welcome to the s!"})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
