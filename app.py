fa406e8bafe4592e14c0fe6add638ade63625e56
Ayush <ayushranjha143@gmail.com>
2025-03-24 18:50:13 +0530

    git

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ CORS(app)
 
 @app.route('/')  # Make sure this route exists
 def home():
-    return jsonify({"message": "Welcome to the s!"})
+    return jsonify({"message": "Welcome to the super!"})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
